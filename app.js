a46a3e526537a7fbe7fba13849104c05ee7fa1b7
Gauripame <gauripame26@gmail.com>
2025-11-04 17:14:54 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//this is js file
 const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
